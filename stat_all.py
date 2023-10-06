4ca8c46e3c01fd1ab877b466b74754adfa27ab90
Austin Appleby <aappleby@gmail.com>
2023-10-06 16:44:09 -0700

    update submodules

--- stat_all.py
+++ stat_all.py
@@ -3,7 +3,7 @@
 import os
 
 print(f"\u001b[38;2;180;255;180m========== METAREPO ==========\u001b[0m")
-os.system(f"git status -s")
+os.system(f"git status -s -b")
 os.system(f"git log --branches --not --remotes")
 os.system("find . -xtype l")
 
@@ -14,7 +14,7 @@ for root, dirs, files in os.walk('.'):
 		if not os.path.exists(dir + "/.git"):
 			continue
 		print(f"\u001b[38;2;180;180;255m========== {dir} ==========\u001b[0m")
-		os.system(f"cd {dir} && git status -s")
+		os.system(f"cd {dir} && git status -s -b")
 		os.system(f"cd {dir} && git log --branches --not --remotes")
 	break
 
