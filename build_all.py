f5f6962b2085a6167b0feaf4c0a128708795215f
Austin Appleby <aappleby@gmail.com>
2023-10-05 18:11:09 -0700

    checkpoint, metrolib rebuilding too much :/

--- build_all.py
+++ build_all.py
@@ -11,11 +11,13 @@ os.system("cd matcheroni && ninja")
 print("\u001b[38;2;180;180;255m========== metron ==========\u001b[0m")
 os.system("cd metron && ./build.py && ninja")
 
+print("\u001b[38;2;180;180;255m========== metroc ==========\u001b[0m")
+os.system("cd metroc && ninja")
+
 
 # aappleby.github.io
 # gb_spu
 # metroboy
-# metroc
 # metronica
 # picorvd
 # pinwheel
