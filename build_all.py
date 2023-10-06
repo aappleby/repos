92547be37f9e2782cdd0c1cc76ada9bc447a0ab5
Austin Appleby <aappleby@gmail.com>
2023-10-05 20:26:17 -0700

    checkpoint

--- build_all.py
+++ build_all.py
@@ -14,14 +14,24 @@ os.system("cd metron && ./build.py && ninja")
 print("\u001b[38;2;180;180;255m========== metroc ==========\u001b[0m")
 os.system("cd metroc && ninja")
 
+print("\u001b[38;2;180;180;255m========== metroboy ==========\u001b[0m")
+os.system("cd metroboy && ninja")
 
-# aappleby.github.io
+print("\u001b[38;2;180;180;255m========== metronica ==========\u001b[0m")
+os.system("cd metronica && ninja")
+
+
+
 # gb_spu
 # metroboy
 # metronica
 # picorvd
 # pinwheel
 # plait
-# rv32iboy
+
+
+
 # smhasher
+# rv32iboy
+# aappleby.github.io
 # wideboard
