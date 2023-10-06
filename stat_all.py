07dcf277b48b8c144ffd41d5e4685093e2796d86
Austin Appleby <aappleby@gmail.com>
2023-10-06 16:51:05 -0700

    make stat_all print one line per repo

--- stat_all.py
+++ stat_all.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 import os
+import sys
 
-print(f"\u001b[38;2;180;255;180m========== METAREPO ==========\u001b[0m")
+print(f"\u001b[38;2;180;255;180mMETAREPO ========================== \u001b[0m", end="")
+sys.stdout.flush()
 os.system(f"git status -s -b")
-os.system(f"git log --branches --not --remotes")
+#os.system(f"git log --branches --not --remotes")
 os.system("find . -xtype l")
 
 for root, dirs, files in os.walk('.'):
@@ -13,9 +15,10 @@ for root, dirs, files in os.walk('.'):
 			continue
 		if not os.path.exists(dir + "/.git"):
 			continue
-		print(f"\u001b[38;2;180;180;255m========== {dir} ==========\u001b[0m")
+		print(f"\u001b[38;2;180;180;255m{(dir + ' ').ljust(35, '=')} \u001b[0m", end="")
+		sys.stdout.flush()
 		os.system(f"cd {dir} && git status -s -b")
-		os.system(f"cd {dir} && git log --branches --not --remotes")
+		#os.system(f"cd {dir} && git log --branches --not --remotes")
 	break
 
 
