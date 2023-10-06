d14688a8fffd218cdc798cb64401aa89d35c7766
Austin Appleby <aappleby@gmail.com>
2023-10-05 18:06:20 -0700

    add broken symlink check

--- stat_all.py
+++ stat_all.py
@@ -5,6 +5,7 @@ import os
 print(f"\u001b[38;2;180;255;180m========== METAREPO ==========\u001b[0m")
 os.system(f"git status -s")
 os.system(f"git log --branches --not --remotes")
+os.system("find . -xtype l")
 
 for root, dirs, files in os.walk('.'):
 	for dir in sorted(dirs):
