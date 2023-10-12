c0d945c45527540ba80d508610494c0f03527df8
Austin Appleby <aappleby@gmail.com>
2023-10-11 19:15:37 -0700

    npm?

--- install_all.sh
+++ install_all.sh
@@ -1,12 +1,12 @@
 git submodule init
 git submodule update
-apt -y install git build-essential ninja-build python3 libicu-dev libsdl2-dev gcc-riscv64-unknown-elf srecord gcc-arm-none-eabi xxd nodejs
+apt -y install git build-essential ninja-build python3 libicu-dev libsdl2-dev gcc-riscv64-unknown-elf srecord gcc-arm-none-eabi xxd nodejs npm
 npm install -g typescript
 
 #./install_emscripten.sh
 emsdk/emsdk install latest
 emsdk/emsdk activate latest
-source emsdk/emsdk_env.sh
+. emsdk/emsdk_env.sh
 
 ./install_icarus.sh
 ./install_icestorm.sh
