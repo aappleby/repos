6800f78f80a77045d0e54b9900b589f8559a03b3
Austin Appleby <aappleby@gmail.com>
2023-12-15 19:55:24 -0800

    update install scripts etc

--- install_all.sh
+++ install_all.sh
@@ -1,16 +1,16 @@
-apt update
-apt -y install git nano
+sudo apt update
+sudo apt -y install git nano 
 # tzdata wants user interaction by default, so we install it first with interaction turned off
-DEBIAN_FRONTEND=noninteractive apt install -y tzdata
-apt -y install git build-essential ninja-build python3 libicu-dev libsdl2-dev gcc-riscv64-unknown-elf srecord gcc-arm-none-eabi xxd nodejs npm
-npm install -g typescript@5.0
+DEBIAN_FRONTEND=noninteractive sudo apt install -y tzdata
+sudo apt -y install gdb build-essential ninja-build python3 libicu-dev libsdl2-dev gcc-riscv64-unknown-elf srecord gcc-arm-none-eabi xxd nodejs npm
+sudo npm install -g typescript@5.0
 
 #./install_emscripten.sh
 emsdk/emsdk install latest
 emsdk/emsdk activate latest
 source emsdk/emsdk_env.sh
 
-./install_icarus.sh
+./install_iverilog.sh
 ./install_icestorm.sh
 ./install_nextpnr.sh
 ./install_verilator.sh
--- install_verilator.sh
+++ install_verilator.sh
@@ -1,6 +1,6 @@
-apt -y install git perl python3 make autoconf g++ flex bison ccache libgoogle-perftools-dev numactl perl-doc libfl2 libfl-dev zlib1g zlib1g-dev help2man
+sudo apt -y install git perl python3 make autoconf g++ flex bison ccache libgoogle-perftools-dev numactl perl-doc libfl2 libfl-dev zlib1g zlib1g-dev help2man
 cd verilator
 autoconf
 ./configure
 make -j$(nproc)
-make install
+sudo make install
