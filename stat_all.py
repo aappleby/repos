91623bfa4e9536ca3641769277d35ffb32d3a5e3
Austin Appleby <aappleby@gmail.com>
2024-02-13 21:54:00 -0800

    make stat_all easier to read

--- stat_all.py
+++ stat_all.py
@@ -11,7 +11,7 @@ def run(tag, cmd):
 	print(ret.decode(), end="")
 
 
-run(f"\u001b[38;2;180;255;180mMETAREPO ========================== \u001b[0m",
+run(f"\u001b[38;2;180;255;180mMETAREPO __________________________ \u001b[0m",
     f"git -c color.status=always status -s -b")
 
 for root, dirs, files in os.walk('.'):
@@ -20,11 +20,11 @@ for root, dirs, files in os.walk('.'):
 			continue
 		if not os.path.exists(dir + "/.git"):
 			continue
-		run(f"\u001b[38;2;180;180;255m{(dir + ' ').ljust(35, '=')} \u001b[0m",
+		run(f"\u001b[38;2;180;180;255m{(dir + ' ').ljust(35, '_')} \u001b[0m",
                     f"cd {dir} && git -c color.status=always status -s -b")
 	break
 
-
+print()
 print("Broken symlinks: ")
 os.system("find . -xtype l")
 print()
