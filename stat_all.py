d81bdee0ccd65fed23c054f56ea6f9239457865d
Austin Appleby <aappleby@gmail.com>
2024-11-11 21:01:50 -0800

    checkpoint

--- stat_all.py
+++ stat_all.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
 
 import os
-import sys
+import glob
 import subprocess
 
 def run(tag, cmd):
-	print(tag, end="");
-	sys.stdout.flush()
-	ret = subprocess.check_output(cmd, shell=True)
-	print(ret.decode(), end="")
+    print(tag, end="")
+    ret = subprocess.check_output(cmd, shell=True)
+    print(ret.decode(), end="")
 
+run("\u001b[38;2;180;255;180mMETAREPO __________________________ \u001b[0m",
+    "git -c color.status=always status -s -b")
 
-run(f"\u001b[38;2;180;255;180mMETAREPO __________________________ \u001b[0m",
-    f"git -c color.status=always status -s -b")
+for dir in sorted(glob.glob("*")):
+    if not os.path.exists(dir + "/.git"): continue
+    run(f"\u001b[38;2;180;180;255m{(dir + ' ').ljust(35, '_')} \u001b[0m",
+                f"cd {dir} && git -c color.status=always status -s -b")
 
-for root, dirs, files in os.walk('.'):
-	#for dir in sorted(dirs):
-    if root == ".":
-        continue
-    if not os.path.exists(root + "/.git"):
-        continue
-    #print(root)
-    run(f"\u001b[38;2;180;180;255m{(root + ' ').ljust(50, '_')} \u001b[0m",
-                f"cd {root} && git -c color.status=always status -s -b")
-	#break
-
-print()
 print("Broken symlinks: ")
 os.system("find . -xtype l")
-print()
+
