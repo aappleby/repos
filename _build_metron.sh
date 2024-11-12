0438e278e858532cc7f46b11922c9c9b326acf95
Austin Appleby <aappleby@gmail.com>
2024-11-11 20:17:02 -0800

    Ditch old build stuff and move external repos to third_party

--- _build_metron.sh
+++
@@ -1,4 +0,0 @@
-apt install git build-essential ninja-build python3 libicu-dev libsdl2-dev
-cd metron
-./build.py
-ninja bin/metron
--- stat_all.py
+++ stat_all.py
@@ -15,14 +15,15 @@ run(f"\u001b[38;2;180;255;180mMETAREPO __________________________ \u001b[0m",
     f"git -c color.status=always status -s -b")
 
 for root, dirs, files in os.walk('.'):
-	for dir in sorted(dirs):
-		if dir.startswith("."):
-			continue
-		if not os.path.exists(dir + "/.git"):
-			continue
-		run(f"\u001b[38;2;180;180;255m{(dir + ' ').ljust(35, '_')} \u001b[0m",
-                    f"cd {dir} && git -c color.status=always status -s -b")
-	break
+	#for dir in sorted(dirs):
+    if root == ".":
+        continue
+    if not os.path.exists(root + "/.git"):
+        continue
+    #print(root)
+    run(f"\u001b[38;2;180;180;255m{(root + ' ').ljust(50, '_')} \u001b[0m",
+                f"cd {root} && git -c color.status=always status -s -b")
+	#break
 
 print()
 print("Broken symlinks: ")
